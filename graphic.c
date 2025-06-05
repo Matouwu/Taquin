af41185738ac3126e4eb32b499a93c3d106e7757
isabelle-zhan <zhanisabellle@gmail.com>
2025-06-05 18:05:59 +0200

    `graphic_init` OK.

--- graphic.c
+++ graphic.c
@@ -9,7 +9,7 @@ void graphic_init(Plateau *p){
     int i,j;
     for(i=0; i < NB_ROW ; i++){
       for(j=0; j < NB_COL; j++){
-        MLV_draw_partial_image(img,IMG_PART*((p->bloc)[i][j].row),IMG_PART*((p->bloc)[i][j].col),IMG_PART-4,IMG_PART-4,j*IMG_PART+4,i*IMG_PART+4);
+        MLV_draw_partial_image(img,IMG_PART*((p->bloc)[i][j].col),IMG_PART*((p->bloc)[i][j].row),IMG_PART-4,IMG_PART-4,j*IMG_PART+4,i*IMG_PART+4);
         if((p->bloc)[i][j].row == 3 && (p->bloc)[i][j].col == 3){
           MLV_draw_filled_rectangle(j*IMG_PART+4, i*IMG_PART+4, IMG_PART-4, IMG_PART-4, MLV_COLOR_BLACK);
         }
